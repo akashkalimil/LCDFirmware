096cda9d77eb64b8aa69bdf0ff736c48e5a3081c
akashkalimili@gmail.com <akashkalimili@gmail.com>
2017-01-07 07:26:25 -0500

    fixsynta

--- main.js
+++ main.js
@@ -12,18 +12,17 @@ lcd.refresh();
 //lcd.Circle(64,80,50,st7735.ST7735_RED);
 lcd.refresh();
 function fillrectangle(x0, y0, x1, y1, color){
- var width = x1 - x0 +1;
- var height = y1 - y0 +1;
- 
-while (y0<= y1){
-    lcd.drawLine(x0,y0,x1,y0,color);
-    y0+=1;
-    
-}
-    lcd.refresh();
-    lcd.sdCSOn();
+         var width = x1 - x0 +1;
+         var height = y1 - y0 +1;
 
-}
+        while (y0<= y1){
+            lcd.drawLine(x0,y0,x1,y0,color);
+            y0+=1;
+
+        }
+
+
+        }
 
 //fillrectangle(0,0,100,100,st7735.ST7735_RED);
 var width = 128;
@@ -42,28 +41,27 @@ function FillCircle (xPos, yPos, radius, color)
  lcd.drawLine(xPos+x,y0,xPos+x,y1,color);
  lcd.drawLine(xPos - x,y0,xPos-x, y1,color);
  }
-     lcd.refresh();
+    // lcd.refresh();
    //s lcd.sdCSOn();
 }
 //lcd.drawCircle(100, 110, 10, st7735.ST7735_BLUE);
 //lcd.drawCircle((width/2)-1,(height/2)-1,60,st7735.ST7735_RED);
-lcd.refresh();
-FillCircle((width/2)+1,(height/2)+1,60,st7735.ST7735_GREEN);
-FillCircle((width/2)+1,(height/2)+1,52,st7735.ST7735_BLACK);
-lcd.refresh();
+//lcd.refresh();
+FillCircle((width/2),(height/2)-1,60,st7735.ST7735_GREEN);
+FillCircle((width/2),(height/2)-1,52,st7735.ST7735_BLACK);
+//lcd.refresh();
 
 function setup_hour(){
 for (var i = 0; i < 360; i += 30) {
     var sx = Math.cos((i - 90) * 0.0174532925);
     var sy = Math.sin((i - 90) * 0.0174532925);
-    var x0 = sx * 56 + (width/2);
-    var yy0 = sy * 56 + (height/2);
-    var x1 = sx * 42 + (width/2);
-    var yy1 = sy * 42 + (height/2);
+    var x0 = sx * 56 + (width/2)-1;
+    var yy0 = sy * 56 + (height/2)-1;
+    var x1 = sx * 42 + (width/2)-1;
+    var yy1 = sy * 42 + (height/2)-1;
 
     lcd.drawLine(x0, yy0, x1, yy1, st7735.ST7735_YELLOW);//garis penanda angka jam
   }
-        lcd.refresh();
 
 }
 
@@ -78,6 +76,7 @@ for (var i = 0; i < 360; i += 30) {
     lcd.refresh();*/
 
 setup_hour();
+lcd.refresh();
 /*
 lcd.refresh();
 

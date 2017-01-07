4e2998a314b874bb24885b6b796b6f1631bccc3d
akashkalimili@gmail.com <akashkalimili@gmail.com>
2017-01-07 07:23:46 -0500

    fixsynta

--- main.js
+++ main.js
@@ -48,8 +48,8 @@ function FillCircle (xPos, yPos, radius, color)
 //lcd.drawCircle(100, 110, 10, st7735.ST7735_BLUE);
 //lcd.drawCircle((width/2)-1,(height/2)-1,60,st7735.ST7735_RED);
 lcd.refresh();
-FillCircle((width/2),(height/2),60,st7735.ST7735_GREEN);
-FillCircle((width/2),(height/2),52,st7735.ST7735_BLACK);
+FillCircle((width/2)+1,(height/2)+1,60,st7735.ST7735_GREEN);
+FillCircle((width/2)+1,(height/2)+1,52,st7735.ST7735_BLACK);
 lcd.refresh();
 
 function setup_hour(){

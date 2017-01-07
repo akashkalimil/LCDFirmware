81300596b7d7445688e64048817491218fc2fc40
akashkalimili@gmail.com <akashkalimili@gmail.com>
2017-01-07 06:55:06 -0500

    fixsynta

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var st7735 = require('jsupm_st7735');
 var lcd = new st7735.ST7735(7, 1, 9, 8);
+
 lcd.fillScreen(st7735.ST7735_RED);
 lcd.refresh();
 lcd.fillScreen(st7735.ST7735_CYAN);
@@ -23,12 +24,31 @@ while (y0<= y1){
     lcd.sdCSOn();
 
 }
+
 //fillrectangle(0,0,100,100,st7735.ST7735_RED);
 var width = 128;
 var height = 160;
+
+
+function FillCircle (xPos, yPos, radius, color)
+// draws filled circle at x,y with given radius & color
+{
+ var r2 = radius * radius;
+ for (var x=0; x<=radius; x++)
+ {
+ var y = Math.sqrt(r2-x*x);
+ var y0 = yPos-y;
+ var y1 = yPos+y;
+ lcd.drawLine(xPos+x,y0,xPos+x,y1,color);
+ lcd.drawLine(xPos - x,y0,xPos-x, y1,color);
+ }
+     lcd.refresh();
+    lcd.sdCSOn();
+}
 //lcd.drawCircle(100, 110, 10, st7735.ST7735_BLUE);
-lcd.drawCircle((width/2)-1,(height/2)-1,60,st7735.ST7735_RED);
+//lcd.drawCircle((width/2)-1,(height/2)-1,60,st7735.ST7735_RED);
 lcd.refresh();
+FillCircle((width/2)-1,(height/2)-1,20,st7735.ST7735_BLUE);
 
 /*
 lcd.refresh();
